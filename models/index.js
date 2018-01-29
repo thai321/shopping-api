534d22a36b3f19d55181197934d16b6a6149fde2
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2018-01-28 23:22:51 -0800

    To heroku

--- models/index.js
+++ models/index.js
@@ -4,12 +4,18 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+let sequelize;
+
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 const models = {
   User: sequelize['import'](path.join(__dirname, 'user')),
